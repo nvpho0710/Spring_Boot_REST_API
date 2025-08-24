87831419ed8941f93d7aa1547da5e3f95e6e5771
nvpho0710 <phonguyenvan71023@gmail.com>
2025-08-24 13:45:03 +0700

    Initial commit

---
+++ spring-be-nvp/src/main/java/com/example/spring_be_nvp/SpringBeNvpApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_be_nvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeNvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeNvpApplication.class, args);
+	}
+
+}
---
+++ spring-be-nvp/src/test/java/com/example/spring_be_nvp/SpringBeNvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_be_nvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeNvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
